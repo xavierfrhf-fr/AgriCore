93d81c2593d9692b7be77a6b6703eb9d14750db1
xavierfrhf-fr <xavier.frhf@gmail.com>
2026-03-24 10:01:15 +0100

    Class Zone - Position ; Enum NomZone + Mapping bdd

---
+++ agricore_back/src/main/java/agricore/model/Position.java
@@ -0,0 +1,66 @@
+package agricore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Position {
+	
+	@Column(nullable = true) // Est-ce que ca peut etre null ? (batiment non placé ? ou alors on le gère différement)
+	private Integer posX;
+	@Column(nullable = true)
+	private Integer posY;
+	@Column(nullable = true)
+	private Integer tailleX;
+	@Column(nullable = true)
+	private Integer tailleY;
+	
+	public Position() {
+	}
+	
+	
+	
+	public Position(Integer posX, Integer posY, Integer tailleX, Integer tailleY) {
+		//Verifier pas d'overlap avec un batiment existant
+		this.posX = posX;
+		this.posY = posY;
+		this.tailleX = tailleX;
+		this.tailleY = tailleY;
+	}
+
+
+	public int getPosX() {
+		return posX;
+	}
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+	public int getPosY() {
+		return posY;
+	}
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+	public int getTailleX() {
+		return tailleX;
+	}
+	public void setTailleX(int tailleX) {
+		this.tailleX = tailleX;
+	}
+	public int getTailleY() {
+		return tailleY;
+	}
+	public void setTailleY(int tailleY) {
+		this.tailleY = tailleY;
+	}
+	
+	public static boolean isPosAvailable(int posX, int posY, int tailleX, int tailleY) {
+		// TODO 
+		// En static car n'a pas besoin qu'un objet soit instancié pour faire la verif
+		// Doit checker (en BDD) si il est possible de placer un batiment à cette position
+		return true;
+	}
+	
+	
+	
+}
