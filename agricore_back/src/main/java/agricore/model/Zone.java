7d0e432fa1db35f9984e26fcfb596a244f2fef4e
xavierfrhf-fr <xavier.frhf@gmail.com>
2026-04-01 09:08:01 +0200

    Add JsonViews Zone

--- agricore_back/src/main/java/agricore/model/Zone.java
+++ agricore_back/src/main/java/agricore/model/Zone.java
@@ -3,6 +3,9 @@ package agricore.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonView;
+
+import agricore.view.Views;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
@@ -22,35 +25,45 @@ public class Zone {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="zone_id")
+	@JsonView(Views.Common.class)
 	private Integer id;
 	
 	@Embedded
+	@JsonView(Views.Common.class)
 	private Position position;
 	
 	@Enumerated(EnumType.STRING) 
 	@Column(nullable = false)
+	@JsonView(Views.Common.class)
 	private NomZone nomZone;
 	
 	
 	//---- Mapping avec les autres classes 
 	@Column(nullable = false)
 	@OneToOne
+	@JsonView(Views.Zone.class)
 	private Fermier fermier;
 	
 	@OneToMany(mappedBy = "zone")
+	@JsonView(Views.ZoneWithAnimal.class)
 	private List<Animal> animals = new ArrayList<>();
 	
+	/*
 	@OneToOne(mappedBy = "zone")
+	@JsonView(Views.Zone.class)
 	private Plante plante;
+	*/
 	
 	@OneToMany(mappedBy = "zone")
+	@JsonView(Views.ZoneWithVehicule.class)
 	private List<Vehicule> vehicules = new ArrayList<>();
 	
 	@OneToMany(mappedBy = "zone")
+	@JsonView(Views.ZoneWithRessource.class)
 	private List<Ressource> ressources = new ArrayList<>();
 	
 	
-	//J'ai pas mis les plantes et animaux, dans constructeur (je pense on les rajoutes par la suite)
+	//J'ai pas mis les plantes et animaux, dans constructeur (je pense on les rajoutes par la suite) mais Spring en auras besoin ??
 	public Zone() {
 	}
 
@@ -100,6 +113,7 @@ public class Zone {
 		this.animals = animals;
 	}
 
+	/*
 	public Plante getPlante() {
 		return plante;
 	}
@@ -107,6 +121,7 @@ public class Zone {
 	public void setPlante(Plante plante) {
 		this.plante = plante;
 	}
+	*/
 	
 	public List<Vehicule> getVehicules() {
 		return vehicules;
@@ -155,7 +170,7 @@ public class Zone {
 	}
 	
 	
-
+/*
 	public boolean planterPlante(Plante p) {
 		if (nomZone.isAutorisePlant()) {
 			plante = p;
@@ -164,7 +179,7 @@ public class Zone {
 		}
 		return false;
 	}
-	
+	*/
 	
 	
 	
