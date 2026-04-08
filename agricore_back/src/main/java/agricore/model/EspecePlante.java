900bb989dc4fa1377a5a99496a7125ad2312e655
Marie <malestro56@gmail.com>
2026-04-08 14:08:29 +0200

    modif methode arroser

--- agricore_back/src/main/java/agricore/model/EspecePlante.java
+++ agricore_back/src/main/java/agricore/model/EspecePlante.java
@@ -1,12 +1,12 @@
 package agricore.model;
 
 public enum EspecePlante {
-	Maïs(5,50000, 7), // le champ pousse en 5 mois, il consomme 50 000L/jour
+	Maïs(5,50000, 7), // le champ pousse en 5 mois, il consomme 50 000L/jour et sa fréquence d'arrosage de base(sans pluie) est tous les 7 jours 
 	Tournesol(5,40000,7), 
 	Blé(8,30000,10), 
 	Colza(10,30000,8);
 
-	private int tempsPousseMois;//en cb de mois pousse l'espèce
+	private int tempsPousseMois;
 	private int consommationEauParJour; //en litres
 	private int frequenceArrosageSansPluie; //valeur statique
 
--- agricore_back/src/main/java/agricore/model/Plante.java
+++ agricore_back/src/main/java/agricore/model/Plante.java
@@ -2,10 +2,15 @@ package agricore.model;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -13,37 +18,136 @@ import jakarta.persistence.Table;
 public class Plante {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="plante_id")
 	private Integer id;
+	@Column(name="date_plantation")
 	private LocalDate datePlantation;
+	@Column(name="date_recolte")
 	private LocalDate dateRecolte;
+	@Enumerated(EnumType.STRING)
 	private EspecePlante espece;
-	private Zone zone;
+	@ManyToOne
+    @JoinColumn(name="zone_id")
+    private Zone zone;
+	@Column(name="dernier_arrosage")
 	private LocalDate dernierArrosage;
-	private double eauDisponible; 
+
 
 
 	public Plante() {}
 
 
-	public Plante(Integer id, LocalDate datePlantation, LocalDate dateRecolte, EspecePlante espece, Zone zone) {
+	
+	public Plante(Integer id, LocalDate datePlantation, LocalDate dateRecolte, EspecePlante espece, Zone zone,
+			LocalDate dernierArrosage) {
 		super();
 		this.id = id;
 		this.datePlantation = datePlantation;
 		this.dateRecolte = dateRecolte;
 		this.espece = espece;
 		this.zone = zone;
+		this.dernierArrosage = dernierArrosage;
 	}
-	public boolean arroser (LocalDate dateArrosage, boolean pluie) {
-		
+
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+
+	public LocalDate getDatePlantation() {
+		return datePlantation;
+	}
+
+
+
+	public LocalDate getDateRecolte() {
+		return dateRecolte;
+	}
+
+
+
+	public EspecePlante getEspece() {
+		return espece;
+	}
+
+
+
+	public Zone getZone() {
+		return zone;
+	}
+
+
+
+	public LocalDate getDernierArrosage() {
+		return dernierArrosage;
+	}
+
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+
+	public void setDatePlantation(LocalDate datePlantation) {
+		this.datePlantation = datePlantation;
+	}
+
+
+
+	public void setDateRecolte(LocalDate dateRecolte) {
+		this.dateRecolte = dateRecolte;
+	}
+
+
+
+	public void setEspece(EspecePlante espece) {
+		this.espece = espece;
+	}
+
+
+
+	public void setZone(Zone zone) {
+		this.zone = zone;
+	}
+
+
+
+	public void setDernierArrosage(LocalDate dernierArrosage) {
+		this.dernierArrosage = dernierArrosage;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Plante [id=" + id + ", datePlantation=" + datePlantation + ", dateRecolte=" + dateRecolte + ", espece="
+				+ espece + ", zone=" + zone + ", dernierArrosage=" + dernierArrosage + "]";
+	}
+
+
+
+	public boolean arroser(LocalDate dateArrosage, boolean pluie) {
+		//je veux que la pluie annule l'arrosage et passe à la date suivante
 		LocalDate dernier = (dernierArrosage != null) ? dernierArrosage : datePlantation;
 	    long joursEcoules = java.time.temporal.ChronoUnit.DAYS.between(dernier, dateArrosage);
+	    int frequence = espece.getFrequenceArrosageSansPluie();
+	    
 	    if (pluie==true) {
-	    	 int frequenceReelle =+ espece.getFrequenceArrosageSansPluie();
+	    	 this.dernierArrosage = dateArrosage;
+	         return true;
 	    }
-	    else {
-	    int frequenceReelle = espece.getFrequenceArrosageSansPluie();
+	    if (joursEcoules >= frequence) {
+	        this.dernierArrosage = dateArrosage;
+	        return true;
 	    }
+	    return false;	
 	}
+	
 	
 	public boolean recolter (LocalDate dateRecolte) {
 		long moisEcoules = java.time.temporal.ChronoUnit.MONTHS.between(datePlantation, dateRecolte);
@@ -60,61 +164,6 @@ public class Plante {
 		return dateRappel;
 	}
 	
-	public Integer getId() {
-		return id;
-	}
-
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-
-	public LocalDate getDatePlantation() {
-		return datePlantation;
-	}
-
-
-	public void setDatePlantation(LocalDate datePlantation) {
-		this.datePlantation = datePlantation;
-	}
-
-
-	public LocalDate getDateRecolte() {
-		return dateRecolte;
-	}
-
-
-	public void setDateRecolte(LocalDate dateRecolte) {
-		this.dateRecolte = dateRecolte;
-	}
-
-
-	public EspecePlante getEspece() {
-		return espece;
-	}
-
-
-	public void setEspece(EspecePlante espece) {
-		this.espece = espece;
-	}
-
-
-	public Zone getZone() {
-		return zone;
-	}
-
-
-	public void setZone(Zone zone) {
-		this.zone = zone;
-	}
-
-
-	@Override
-	public String toString() {
-		return "Plante [id=" + id + ", datePlantation=" + datePlantation + ", dateRecolte=" + dateRecolte
-				+ ", espece=" + espece + ", zone=" + zone + "]";
-	}
-
+	
 
 }
