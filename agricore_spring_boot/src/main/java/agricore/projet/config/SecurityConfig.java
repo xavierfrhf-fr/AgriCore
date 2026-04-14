1e8b197f2a22bd77db5ef07b753c34e595a823ef
Tim <timothy.labidi@gmail.com>
2026-04-14 11:15:50 +0200

    ajout du ignor crsf dans le SecurityConfig

--- agricore_spring_boot/src/main/java/agricore/projet/config/SecurityConfig.java
+++ agricore_spring_boot/src/main/java/agricore/projet/config/SecurityConfig.java
@@ -1,5 +1,18 @@
 package agricore.projet.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
 
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf -> csrf.ignoringRequestMatchers("/api/**"));
+        return http.build();
+    }
+    
 }
