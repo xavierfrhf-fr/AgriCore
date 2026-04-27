cc3d93c0f61938ba78a8cf8669c9bf4b7efc2c42
xavierfrhf-fr <xavier.frhf@gmail.com>
2026-04-27 13:50:49 +0200

    CREATION POUR DATAS(Enum) VERS FRONT
    Creation DataController + package dto.data
    Ajout de la DTO + methode dans le controller pour ZoneData

---
+++ agricore_spring_boot/src/main/java/agricore/projet/controller/DataController.java
@@ -0,0 +1,24 @@
+package agricore.projet.controller;
+
+import agricore.projet.dto.data.ZoneDataDTO;
+import agricore.projet.model.zone.NomZone;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/data")
+public class DataController {
+
+    @GetMapping("/zone")
+    public List<ZoneDataDTO> getZoneData(){
+        List<ZoneDataDTO> dtos = new ArrayList<>();
+        for (NomZone zone : NomZone.values()){
+            dtos.add(ZoneDataDTO.from(zone));
+        }
+        return dtos;
+    }
+}
