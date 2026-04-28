8365cf6ee127555aaf372f6323eaea78bf20ab5f
ihecker <irwin.hecker@outlook.com>
2026-04-28 15:04:16 +0200

    CHORE - @JSONVIEW RESTANTES SUPPRIMEES

--- agricore_spring_boot/src/main/java/agricore/projet/model/Fermier.java
+++ agricore_spring_boot/src/main/java/agricore/projet/model/Fermier.java
@@ -3,24 +3,20 @@ package agricore.projet.model;
 import java.util.List;
 
 import agricore.projet.model.zone.Zone;
-import com.fasterxml.jackson.annotation.JsonView;
-
-import agricore.projet.view.Views;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 
 @Entity
 public class Fermier extends Utilisateur {
-	
-	@OneToMany(mappedBy="fermier")
-	@JsonView(Views.FermierWithEmploye.class)
+
+	@OneToMany(mappedBy = "fermier")
 	private List<Employe> employe;
-	
-	@OneToMany(mappedBy="fermier")
-	@JsonView(Views.FermierWithZone.class)
+
+	@OneToMany(mappedBy = "fermier")
 	private List<Zone> zone;
-	
-	public Fermier() {}
+
+	public Fermier() {
+	}
 
 	public Fermier(Integer id, String login, String password) {
 		super(id, login, password);
@@ -30,7 +26,5 @@ public class Fermier extends Utilisateur {
 	public String toString() {
 		return "Fermier [id=" + id + ", login=" + login + ", password=" + password + "]";
 	}
-	
-	
 
 }
