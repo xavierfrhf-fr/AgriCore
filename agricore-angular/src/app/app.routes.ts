46deb0981be305d1818546e61cd31e486f4559c3
Tim <timothy.labidi@gmail.com>
2026-05-02 15:45:59 +0200

    Ajout du formulaire de connexion

--- agricore-angular/src/app/app.routes.ts
+++ agricore-angular/src/app/app.routes.ts
@@ -2,8 +2,11 @@ import {Routes} from '@angular/router';
 
 import {RessourcePage} from './page/ressource-page/ressource-page';
 import { ZonePage } from './page/zone/zone-page/zone-page';
+import { AccueilPage } from './page/accueil-page/accueil-page';
 
 export const routes: Routes = [
+  {path: '', redirectTo: 'accueil', pathMatch: 'full'},
+  {path: 'accueil', component: AccueilPage, title: "Page d'acceuil"},
   {path: 'ressource', component: RessourcePage, title: 'Gestion des ressources'},
   {path: 'zone', component: ZonePage, title: 'Gestion des zones'}
 ];
