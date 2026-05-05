8cea9472b833c99f389300293300d20ef2fae634
AxelleDrd <axelle.drouard@orange.fr>
2026-05-05 13:11:17 +0200

    Modif enum Animal + sprite animaux

--- agricore_spring_boot/src/main/java/agricore/projet/model/animal/EspeceAnimal.java
+++ agricore_spring_boot/src/main/java/agricore/projet/model/animal/EspeceAnimal.java
@@ -8,6 +8,7 @@ public enum EspeceAnimal {
 	COCHON(CheminAsset.ANIMAL + "cochon.png",NomZone.ETABLE, AnimalProduct.sameForMaleAndFemale(null, "Cochon")),
 	VACHE(CheminAsset.ANIMAL + "vache.png",NomZone.ETABLE, AnimalProduct.differentForMaleAndFemale(NomRessource.LAIT_VACHE, "Vache", null, "Taureau")),
 	MOUTON(CheminAsset.ANIMAL + "mouton.png",NomZone.ETABLE, AnimalProduct.differentForMaleAndFemale(NomRessource.LAIT_BREBIS, "Brebis", NomRessource.LAINE, "Mouton")),
+	CHEVRE(CheminAsset.ANIMAL + "chevre.png", NomZone.ETABLE, AnimalProduct.differentForMaleAndFemale(NomRessource.LAIT_CHEVRE, "Chèvre", null, "Bouc")),
 	POULE(CheminAsset.ANIMAL + "poule.png",NomZone.POULAILLER, AnimalProduct.differentForMaleAndFemale(NomRessource.OEUF_POULE, "Poule", null, "Coq")),
 	CANNE(CheminAsset.ANIMAL + "canne.png",NomZone.ETABLE, AnimalProduct.differentForMaleAndFemale(NomRessource.OEUF_CANNE, "Canne", NomRessource.PLUME, "Canard")),
 	OIE(CheminAsset.ANIMAL + "oie.png",NomZone.ETABLE, AnimalProduct.differentForMaleAndFemale(NomRessource.OEUF_OIE, "Oie", NomRessource.PLUME, "Jars")),
