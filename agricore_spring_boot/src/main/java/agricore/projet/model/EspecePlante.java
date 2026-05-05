d499dcb9748d3cdfb3bfee5d0ca407d31ea9712d
Tim <timothy.labidi@gmail.com>
2026-05-05 17:34:05 +0200

    merge

--- agricore_spring_boot/src/main/java/agricore/projet/model/EspecePlante.java
+++ agricore_spring_boot/src/main/java/agricore/projet/model/EspecePlante.java
@@ -3,21 +3,15 @@ package agricore.projet.model;
 import agricore.projet.model.zone.NomZone;
 
 public enum EspecePlante {
-	Maïs(5, 0.05, TypeVehicule.TRACTEUR), // le champ pousse en 5 mois, il consomme 50 000L/jour et sa fréquence
-	Maïs(5, 0.05, NomZone.CHAMPS), // le champ pousse en 5 mois, il consomme 50 000L/jour et sa fréquence
+	Maïs(5, 0.05, NomZone.CHAMPS, TypeVehicule.TRACTEUR), // le champ pousse en 5 mois, il consomme 50 000L/jour et sa fréquence
 					// d'arrosage de base(sans pluie) est tous les 7 jours
-	Tournesol(5, 0.02,TypeVehicule.TRACTEUR),
-	Blé(8, 0.01, TypeVehicule.MOISSONNEUSE),
-	Colza(10, 0.01, TypeVehicule.MOISSONNEUSE),
-	Pommier(10, 0.01, TypeVehicule.TRACTEUR),
-	Fraisier(10, 0.01, TypeVehicule.PICKUP),
-	Poirier(10, 0.01, TypeVehicule.UTILITAIRE);
-	Tournesol(5, 0.02, NomZone.CHAMPS),
-	Blé(8, 0.01, NomZone.CHAMPS),
-	Colza(10, 0.01, NomZone.CHAMPS),
-	Pommier(10, 0.01, NomZone.CHAMPS),
-	Fraisier(10, 0.01, NomZone.CHAMPS),
-	Poirier(10, 0.01, NomZone.CHAMPS);
+	Tournesol(5, 0.02, NomZone.CHAMPS,TypeVehicule.TRACTEUR),
+	Blé(8, 0.01, NomZone.CHAMPS, TypeVehicule.MOISSONNEUSE),
+	Colza(10, 0.01, NomZone.CHAMPS, TypeVehicule.MOISSONNEUSE),
+	Pommier(10, 0.01, NomZone.CHAMPS, TypeVehicule.TRACTEUR),
+	Fraisier(10, 0.01, NomZone.CHAMPS, TypeVehicule.PICKUP),
+	Poirier(10, 0.01, NomZone.CHAMPS, TypeVehicule.UTILITAIRE);
+	
 
 	private int tempsPousseMois;
 	private double consommationEauParMin; // en litres
@@ -25,8 +19,7 @@ public enum EspecePlante {
 
 	public TypeVehicule vehiculeRequis;
 
-	private EspecePlante(int tempsPousseMois, double consommationEauParMin, TypeVehicule vehiculeRequis) {
-	private EspecePlante(int tempsPousseMois, double consommationEauParMin, NomZone allowedZone) {
+	private EspecePlante(int tempsPousseMois, double consommationEauParMin, NomZone allowedZone, TypeVehicule vehiculeRequis) {
 		this.tempsPousseMois = tempsPousseMois;
 		this.consommationEauParMin = consommationEauParMin;
 		this.vehiculeRequis = vehiculeRequis;
