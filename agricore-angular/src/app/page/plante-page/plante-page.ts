baa0a3d49cb48bada85aad64a78303b8597c6d95
Marie <malestro56@gmail.com>
2026-05-06 15:29:26 +0200

    Structure plante front

---
+++ agricore-angular/src/app/page/plante-page/plante-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plante-page',
+  imports: [],
+  templateUrl: './plante-page.html',
+  styleUrl: './plante-page.css',
+})
+export class PlantePage {}
