75b6dac6d92cebdd428e67d38d6c803acf8db412
ihecker <irwin.hecker@outlook.com>
2026-05-06 17:53:56 +0200

    CHORE - TYPO

--- agricore-angular/src/app/app.routes.ts
+++ agricore-angular/src/app/app.routes.ts
@@ -1,15 +1,18 @@
 import {Routes} from '@angular/router';
 
+import {AccueilPage} from './page/accueil-page/accueil-page';
+import {AnimalPage} from './page/animal/animal-page/animal-page';
 import {RessourcePage} from './page/ressource-page/ressource-page';
-import { ZonePage } from './page/zone/zone-page/zone-page';
-import { AccueilPage } from './page/accueil-page/accueil-page';
-import { AnimalPage } from './page/animal/animal-page/animal-page';
-import { VehiculePage } from './page/vehicule-page/vehicule-page';
+import {VehiculePage} from './page/vehicule-page/vehicule-page';
+import {ZonePage} from './page/zone/zone-page/zone-page';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'accueil', pathMatch: 'full'},
-  {path: 'accueil', component: AccueilPage, title: "Page d'acceuil"},
-  {path: 'ressource', component: RessourcePage, title: 'Gestion des ressources'},
+  {path: 'accueil', component: AccueilPage, title: 'Page d\'accueil'}, {
+    path: 'ressource',
+    component: RessourcePage,
+    title: 'Gestion des ressources'
+  },
   {path: 'zone', component: ZonePage, title: 'Gestion des zones'},
   {path: 'animal', component: AnimalPage, title: 'Gestion des animaux'},
   {path: 'vehicule', component: VehiculePage, title: 'Gestion des véhicules'}
