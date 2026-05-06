aea220c39e7d30eb9d4da1db7e0476feae780e95
xavierfrhf-fr <xavier.frhf@gmail.com>
2026-05-06 10:13:19 +0200

    Création DTO

---
+++ agricore_spring_boot/src/main/java/agricore/projet/dto/data/TransformationDataDTO.java
@@ -0,0 +1,15 @@
+package agricore.projet.dto.data;
+
+import agricore.projet.model.ressource.Transformation;
+import agricore.projet.model.zone.NomZone;
+
+import java.util.List;
+
+public record TransformationDataDTO(
+        Transformation transformation,
+        NomZone requiredZone,
+        boolean zoneExist,
+        int maxTransfoPossible,
+        List<TransformationPartDTO> ingredients,
+        List<TransformationPartDTO> produits
+) {}
